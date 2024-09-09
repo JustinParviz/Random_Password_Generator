4b093fd8e28458bae7905cf990a53df284f11bc3
JustinParviz <jarshravan@gmail.com>
2024-09-09 16:54:55 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Random Password Generator
+
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
+
+    return "";
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, 
+                                  includeLowercase, 
+                                  includeUppercase, 
+                                  includeNumbers, 
+                                  includeSymbols);
+
+console.log(`Generated password: ${password}`);
